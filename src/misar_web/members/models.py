b1d3f786890dc50a76f52cfbe2f4c3780a6c2aef
akthe-at <arkelly2@medicine.wisc.edu>
2023-11-25 06:39:37 -0600

    initial work on membership model

--- src/misar_web/members/models.py
+++ src/misar_web/members/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    """Defines a member of the organization"""
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    date_of_birth = models.DateField(default=False)
+    member_since = models.DateField(default=False)
+    website_join_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "members"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
