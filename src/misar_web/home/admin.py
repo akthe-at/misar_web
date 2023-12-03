4345cd1ab139cd001893da1850714177f08b036b
Adam K <arkelly2@medicine.wisc.edu>
2023-12-03 07:06:39 -0600

    register teamgoals with admin page

--- src/misar_web/home/admin.py
+++ src/misar_web/home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SiteInfo
+from .models import SiteInfo, TeamGoals
 # Register your models here.
 
-admin.site.register(SiteInfo)
+admin.site.register([SiteInfo, TeamGoals])
+
