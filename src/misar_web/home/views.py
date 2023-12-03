a4f6c1c1e482a2dd88134e64fe6629eed5134730
Adam K <arkelly2@medicine.wisc.edu>
2023-12-03 07:07:55 -0600

    create views for contact/donation pages

--- src/misar_web/home/views.py
+++ src/misar_web/home/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
-from .models import SiteInfo
+from .models import SiteInfo, TeamGoals
 
 
 def home(request: HttpRequest):
     site_info = SiteInfo.objects.get(id=1)
-    return render(
-        request, template_name="home/home.html", context={"siteinfo": site_info}
-    )
+    team_goals = TeamGoals.objects.all()
+    context = {"siteinfo": site_info, "team_goals": team_goals}
+    return render(request=request, template_name="home/home.html", context=context)
 
 
 def donate(request: HttpRequest):
-    return render(request, template_name="home/donate.html", context={"donate": donate})
+    context = {"donate": donate}
+    return render(request=request, template_name="home/donate.html", context=context)
+
+
+def contact(request: HttpRequest):
+    site_info = SiteInfo.objects.get(id=1)
+    context = {"siteinfo": site_info}
+    return render(request=request, template_name="home/contact.html", context=context)
 
