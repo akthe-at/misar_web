6d08e79b6dd5c66b0add900538fcb6f79b91db28
Adam K <arkelly2@medicine.wisc.edu>
2023-12-08 05:21:36 -0600

    fixing/adding urls

--- src/misar_web/members/urls.py
+++ src/misar_web/members/urls.py
@@ -1,13 +1,22 @@
 """Defines URL patterns for members"""
 
 from django.urls import path
+
+
 from . import views
-from .views import MemberRegisterView
 
 urlpatterns = [
-    path("register/", MemberRegisterView.as_view(), name="register"),
-    path("member_home/", views.member_landing, name="member_home"),
-    path("files/", views.file_list, name="member_files"),
-    path("upload_file/", views.file_upload, name="upload_file"),
+    path("register/", views.MemberRegisterView.as_view(), name="register"),
+    path(
+        "member_home/",
+        views.member_home,
+        name="member_home",
+    ),
+    path(
+        "files/",
+        views.files,
+        name="files",
+    ),
+    path("upload/", views.file_upload, name="upload"),
 ]
 
