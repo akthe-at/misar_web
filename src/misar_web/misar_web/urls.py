4b9c9584f2ab46e653e57763fc5b7d1f427c18c5
Adam K <arkelly2@medicine.wisc.edu>
2023-12-08 05:19:40 -0600

    serve media/static files during dev

--- src/misar_web/misar_web/urls.py
+++ src/misar_web/misar_web/urls.py
@@ -25,5 +25,9 @@ urlpatterns = [
     path("members/", include("django.contrib.auth.urls")),
     path("members/", include("members.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
