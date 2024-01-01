62924331ba8583574283fb61bc9f4a7c3dd006a6
Adam K <arkelly2@medicine.wisc.edu>
2023-12-31 21:11:22 -0600

    toying with locations and events for a calendar

--- src/misar_web/members/models.py
+++ src/misar_web/members/models.py
@@ -70,3 +70,56 @@ class MemberFile(models.Model):
 
         super().save(*args, **kwargs)
 
+
+class Location(models.Model):
+    """A class to represent the location for events such as trainings, demos, fundraisers, etc."""
+
+    location_point_of_contact = models.CharField(
+        ("Location Point of Contact, if exists."), max_length=50, null=True
+    )
+    location_phone_number = models.CharField(
+        ("Location Phone Number"), max_length=50, null=True
+    )
+    location_email = models.EmailField(("Location Email"), blank=True)
+    location_name = models.CharField(("Location Name"), max_length=50)
+    location_website = models.URLField(("Location Website"), max_length=75)
+    location_description = models.CharField(("Location Description"), max_length=50)
+    location_address = models.CharField(("Location Address"), max_length=100)
+    location_city = models.CharField(("Location City"), max_length=50)
+    location_state = models.CharField(("Location State"), max_length=50)
+    location_zip_code = USZipCodeField(("Location Zip Code"), null=False, max_length=10)
+
+    def __str__(self):
+        return self.location_name
+
+
+class Event(models.Model):
+    """A class to represent events"""
+
+    event_poster = models.ForeignKey(
+        Member, on_delete=models.CASCADE, related_name="owned_events", null=False
+    )
+    event_organizer = models.ForeignKey(
+        Member, on_delete=models.CASCADE, related_name="organized_events", null=True
+    )
+    event_name = models.CharField(("Event Name"), max_length=50)
+    description = models.CharField(("Event Description"), max_length=50)
+    event_type = models.CharField(("Event Type"), max_length=50)
+    date = models.DateTimeField(("Event Date"))
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, related_name="event_location", null=True
+    )
+    address = models.CharField(("Event Address"), max_length=50)
+    city = models.CharField(("Event City"), max_length=50)
+    state = models.CharField(("Event State"), max_length=50)
+    zip_code = USZipCodeField(("Event Zip Code"), null=False, max_length=10)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(auto_now_add=True)
+    special_instructions = models.TextField(("Special Instructions"), max_length=500)
+    attendees = models.ManyToManyField(
+        Member, related_name="event_attendees", blank=True
+    )
+
+    def __str__(self):
+        return self.event_name
+
