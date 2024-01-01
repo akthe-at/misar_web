428383e39a84f7974cfb1e6550cf6264a5e90dbb
Adam K <arkelly2@medicine.wisc.edu>
2023-12-31 21:11:30 -0600

    adding a url for the calendar

--- src/misar_web/members/urls.py
+++ src/misar_web/members/urls.py
@@ -44,8 +44,8 @@ urlpatterns = [
         name="password_reset_complete",
     ),
     path("delete_file/<int:file_id>/", views.delete_file, name="delete_file"),
-    # path("upload/", views.file_upload, name="upload"),
     path("share/<int:file_id>/", ShareFileView.as_view(), name="share"),
     path("download/<int:file_id>/", views.download_file, name="download"),
+    path("<int:year>/<str:month>/", views.team_calendar, name="team_calendar"),
 ]
 
