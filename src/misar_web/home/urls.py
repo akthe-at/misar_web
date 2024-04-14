0445ed636a56285a18f9a3c7c0faacd0b880cc6d
Adam K. <arkelly2@medicine.wisc.edu>
2024-04-14 06:10:32 -0500

    add url for specialty icon modal

--- src/misar_web/home/urls.py
+++ src/misar_web/home/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("<int:spec_id>/", views.icon_modal, name="icon_modal"),
     path("donate", views.donate, name="donate"),
     path("contact", views.contact, name="contact"),
     path("about", views.about, name="about"),
 ]
-
