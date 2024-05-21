a92dd9c2aa231a802259af285352e0a7d3c707d6
AKthe-AT <arkelly111@gmail.com>
2024-05-21 15:38:27 +0000

    settings and url fix

--- src/misar_web/misar_web/settings.py
+++ src/misar_web/misar_web/settings.py
@@ -26,7 +26,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
