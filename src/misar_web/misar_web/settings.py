f5f7724139e47de390af588d4d86ee3691711466
Adam K. <arkelly2@medicine.wisc.edu>
2024-06-11 22:29:21 -0500

    fix static_url, extra forward slash

--- src/misar_web/misar_web/settings.py
+++ src/misar_web/misar_web/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 if not DEBUG:
     # STATIC_ROOT = BASE_DIR / "staticfiles"
