d91058eb694647f29cb2f948a1470f29151fc1e8
Adam K. <arkelly2@medicine.wisc.edu>
2024-07-22 10:52:45 -0500

    add robots.txt

--- src/misar_web/home/urls.py
+++ src/misar_web/home/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
     path("<int:spec_id>/", views.icon_modal, name="icon_modal"),
     path("donate", views.donate, name="donate"),
     path("contact", views.contact, name="contact"),
